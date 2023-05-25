23cb486e6021fb5bc3a2575a62ef095ef51cefd3
mohitjangra117 <131031018+mohitjangra117@users.noreply.github.com>
2023-05-25 05:51:33 +0530

    Add files via upload

---
+++ t1.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int main()
+
+{
+
+int day;
+printf("Enter the day:");
+scanf("%d",&day);
+
+switch(day) {
+    case 1: 
+    printf("Sunday");
+    break;
+
+    case 2: 
+    printf("Monday");
+    break;
+
+    case 3: 
+    printf("Tuesday");
+    break;
+
+    case 4: 
+    printf("Wednesday");
+    break;
+
+    case 5: 
+    printf("Thursday");
+    break;
+
+    case 6: 
+    printf("Friday");
+    break;
+
+    case 7: 
+    printf("Saturday");
+    break;
+
+    default:
+    printf("error");
+}
+
+return 0;
+
+}
